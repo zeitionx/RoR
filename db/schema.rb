502bece79d08537549b939439b56d25b47907528
nikko.mayo <nikko.mayo@paramount.com.ph>
2018-07-03 15:09:05 +0800

    chapter12-and-13

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180628101213) do
+ActiveRecord::Schema.define(version: 20180702024014) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
