357b060f6179df599943dc3273d77f23cb0f8a4c
nikko.mayo <nikko.mayo@paramount.com.ph>
2018-07-04 15:15:44 +0800

    chapter 14 logging in

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,11 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
-
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+class ActionDispatch::IntegrationTest
+  def login_as(user)
+    post login_url, params: { name: user.name, password: 'secret' }
+  end
+  def logout
+    delete logout_url
+  end
+  def setup
+    login_as users(:one)
+  end
 end
