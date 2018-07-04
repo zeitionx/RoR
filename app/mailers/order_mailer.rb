1a274f88b097cb26758f784d7038c58334cab5fc
nikko.mayo <nikko.mayo@paramount.com.ph>
2018-07-04 08:50:29 +0800

    chapter-13

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,23 +1,24 @@
 class OrderMailer < ApplicationMailer
   default from: 'Sam Ruby <depot@example.com>'
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.order_mailer.received.subject
   #
-  def received(order)
-    @order = order
-    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
-    end
+  def received
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.order_mailer.shipped.subject
   #
-  def shipped(order)
-    @order = order
-    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
   end
 end
